ceea67caf9c131f6468b039f164b9412d9617989
thanhdat12a7cntt-debug <thanhdat12a7cntt@gmail.com>
2026-03-10 21:18:09 +0700

    Temp

---
+++ BookApp_temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookApp_temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Model.Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Model.Category>().HasData(
+                new Model.Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Model.Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Model.Category { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
+        }
+    }
+}
